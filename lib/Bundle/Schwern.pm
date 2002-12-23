83fb54cee5ceb208cd91f9d0982e98df8a68ea00
Michael G Schwern <mschwern@cpan.org>
2002-12-23 12:58:26 +0000

    Import of MSCHWERN/Bundle-Schwern-0.01 from CPAN.
    gitpan-cpan-distribution: Bundle-Schwern
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         MSCHWERN/Bundle-Schwern-0.01.tar.gz
    gitpan-cpan-author:       MSCHWERN
    gitpan-cpan-maturity:     released

---
+++ lib/Bundle/Schwern.pm
@@ -0,0 +1,39 @@
+package Bundle::Schwern;
+
+$VERSION = 0.01;
+
+
+=head1 NAME
+
+Bundle::Schwern - A bunch of modules Schwern likes.
+
+=head1 SYNOPSIS
+
+  perl -MCPAN -e install Bundle::Schwern
+
+=head1 DESCRIPTION
+
+This is just a bundle so I can quickly install a bunch of little
+modules I like to have around.
+
+=head1 CONTENTS
+
+Archive::Any
+Carp::Assert
+CLASS
+Class::Accessor
+Class::Data::Inheritable
+Exporter::Lite
+File::chdir
+Module::Info
+UNIVERSAL::requirea
+YAML
+
+=head1 AUTHOR
+
+Michael G Schwern <schwern@pobox.com>
+
+=cut
+
+1;
+
